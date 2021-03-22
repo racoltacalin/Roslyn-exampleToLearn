535d0b3f8b1b40a8ab205ea0b6db5bc1e2226de6
Clain <Calin.Racolta@student.utcluj.ro>
2021-03-22 15:54:19 +0200

    add Syntax Walker

---
+++ SyntaxWalker/SyntaxWalker/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxWalker
+{
+    class Program
+    {
+        public static void Main()
+        {
+            const string programText =
+                @"using System;
+                using System.Collections.Generic;
+                using System.Linq;
+                using System.Text;
+                using Microsoft.CodeAnalysis;
+                using Microsoft.CodeAnalysis.CSharp;
+
+                namespace TopLevel
+                {
+                    using Microsoft;
+                    using System.ComponentModel;
+
+                    namespace Child1
+                    {
+                        using Microsoft.Win32;
+                        using System.Runtime.InteropServices;
+
+                        class Foo { }
+                    }
+
+                    namespace Child2
+                    {
+                        using System.CodeDom;
+                        using Microsoft.CSharp;
+
+                        class Bar { }
+                    }
+                }";
+
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(programText);
+            CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+
+            var collector = new UsingCollector();
+            collector.Visit(root);
+            foreach (var directive in collector.Usings)
+            {
+                Console.WriteLine(directive.Name);
+            }
+        }
+    }
+}
---
+++ SyntaxWalker/SyntaxWalker/UsingCollector.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxWalker
+{
+    public class UsingCollector : CSharpSyntaxWalker
+    {
+        public ICollection<UsingDirectiveSyntax> Usings { get; } = new List<UsingDirectiveSyntax>();
+
+        public override void VisitUsingDirective(UsingDirectiveSyntax node)
+        {
+            Console.WriteLine($"\tVisitUsingDirective called with {node.Name}.");
+            if (node.Name.ToString() != "System" &&
+                !node.Name.ToString().StartsWith("System."))
+            {
+                Console.WriteLine($"\t\tSuccess. Adding {node.Name}.");
+                this.Usings.Add(node);
+            }
+        }
+    }
+}
