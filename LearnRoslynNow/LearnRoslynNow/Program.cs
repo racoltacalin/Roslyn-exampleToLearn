d2c28246798abb236a5ceae83472d645ac18850c
Clain <Calin.Racolta@student.utcluj.ro>
2021-03-22 15:56:36 +0200

    the syntax tree api

---
+++ LearnRoslynNow/LearnRoslynNow/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Linq;
+
+namespace LearnRoslynNow
+{
+    class Program
+    {
+        static void Main()
+        {
+            var tree = CSharpSyntaxTree.ParseText(@"
+            class C
+            {
+                void Method()
+                {
+                    try
+                    {
+                    //this is a blog
+                    }
+                    catch(Exception)
+                    {
+                    }
+                }
+            }");
+
+            /// Checking for errors in our "code"
+            //var diagnostics = tree.GetDiagnostics().Where(n => n.Severity == Microsoft.CodeAnalysis.DiagnosticSeverity.Error).First();
+            //Console.WriteLine(diagnostics);
+
+            Console.WriteLine("---------------------------------------");
+
+            var root = tree.GetRoot();
+            Console.WriteLine(root);
+
+            Console.WriteLine("---------------------------------------");
+
+            var method = root.DescendantNodes().OfType<MethodDeclarationSyntax>().First();
+            Console.WriteLine(method);
+
+            Console.WriteLine("---------------------------------------");
+
+            var tryStatement = root.DescendantNodes().OfType<TryStatementSyntax>().First();
+            Console.WriteLine(tryStatement);
+
+            Console.WriteLine("---------------------------------------");
+
+            var block = tryStatement.Block;
+            Console.WriteLine(block);
+
+            Console.WriteLine("---------------------------------------");
+
+            var returnType = SyntaxFactory.ParseTypeName("string");
+            Console.WriteLine(returnType);
+
+            Console.WriteLine("---------------------------------------");
+
+            /// You need to create a newMethod to change the name of method (for example...)
+            var newMethod = method.WithReturnType(returnType);
+            Console.WriteLine(newMethod);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LearnRoslynNow/SyntaxTreeVisitor/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxTreeVisitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var walker = new MyWalker();
+            var tree = CSharpSyntaxTree.ParseText(@"
+            class C
+            {
+                void M()
+                {
+                }
+            }");
+
+            var root = tree.GetRoot();
+            walker.Visit(root);
+            var result = walker.sb.ToString();
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+
+    public class MyWalker : CSharpSyntaxWalker
+    {
+        public MyWalker()
+            : base(SyntaxWalkerDepth.Token)
+        {
+
+        }
+
+        public StringBuilder sb = new StringBuilder();
+
+        //public override void VisitToken(SyntaxToken token)
+        //{
+        //    //sb.Append(token.ToFullString());
+        //    sb.Append(token.ToString());
+        //}
+
+        public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            sb.Append(node.ToString());
+            base.VisitMethodDeclaration(node);
+        }
+    }
+}
