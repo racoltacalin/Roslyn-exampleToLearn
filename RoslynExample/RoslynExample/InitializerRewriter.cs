4593dad66e142b65d40bc83571621868335b7238
Clain <Calin.Racolta@student.utcluj.ro>
2021-03-23 11:50:50 +0200

    roslyn - good one

---
+++ RoslynExample/RoslynExample/InitializerRewriter.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynExample
+{
+    public class InitializerRewriter : CSharpSyntaxRewriter
+    {
+        private readonly SemanticModel SemanticModel;
+
+        public InitializerRewriter(SemanticModel semanticModel)
+        {
+            this.SemanticModel = semanticModel;
+        }
+
+        public override SyntaxNode VisitVariableDeclaration(VariableDeclarationSyntax node)
+        {
+            // determination of the type of the variable(s)
+            var typeSymbol = (ITypeSymbol)this.SemanticModel.GetSymbolInfo(node.Type).Symbol;
+
+            bool changed = false;
+
+            // you could declare more than one variable with one expression
+            SeparatedSyntaxList<VariableDeclaratorSyntax> vs = node.Variables;
+
+            // we create a space to improve readability
+            SyntaxTrivia space = SyntaxFactory.SyntaxTrivia(SyntaxKind.WhitespaceTrivia, " ");
+            for (var i = 0; i < node.Variables.Count; i++)
+            {
+                // there is not an initialization
+                if (this.SemanticModel.GetSymbolInfo(node.Type).Symbol.ToString() == "int" &&
+                    node.Variables[i].Initializer == null)
+                {
+                    // we create a new espression "42"
+                    // preceded by the space we create earlier
+                    ExpressionSyntax es = SyntaxFactory.ParseExpression("42")
+                                                       .WithLeadingTrivia(space);
+
+                    // basically we create an assignment to the espression we just created
+                    EqualsValueClauseSyntax evc = SyntaxFactory.EqualsValueClause(es)
+                                                               .WithLeadingTrivia(space);
+
+                    // we replace the null initializer with ours
+                    vs = vs.Replace(vs.ElementAt(i), vs.ElementAt(i).WithInitializer(evc));
+                    changed = true;
+                }
+
+                // there is an initialization but it's not to 42
+                if (this.SemanticModel.GetSymbolInfo(node.Type).Symbol.ToString() == "int" &&
+                    node.Variables[i].Initializer != null &&
+                    !node.Variables[i].Initializer.Value.IsEquivalentTo(SyntaxFactory.ParseExpression("42")))
+                {
+                    ExpressionSyntax es = SyntaxFactory.ParseExpression("42")
+                                                       .WithLeadingTrivia(space);
+
+                    EqualsValueClauseSyntax evc = SyntaxFactory.EqualsValueClause(es);
+                    vs = vs.Replace(vs.ElementAt(i), vs.ElementAt(i).WithInitializer(evc));
+                    changed = true;
+                }
+            }
+
+            if (changed == true)
+            {
+                return node.WithVariables(vs);
+            }
+            return base.VisitVariableDeclaration(node);
+        }
+    }
+}
+
---
+++ RoslynExample/RoslynExample/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynExample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Compilation test = CreateTestCompilation();
+            foreach (SyntaxTree sourceTree in test.SyntaxTrees)
+            {
+                // creation of the semantic model
+                SemanticModel model = test.GetSemanticModel(sourceTree);
+                // initialization of our rewriter class
+                InitializerRewriter rewriter = new InitializerRewriter(model);
+                // analysis of the tree
+                SyntaxNode newSource = rewriter.Visit(sourceTree.GetRoot());
+                if (!Directory.Exists(@"../new_src"))
+                    Directory.CreateDirectory(@"../new_src");
+                // if we changed the tree we save a new file
+                if (newSource != sourceTree.GetRoot())
+                {
+                    File.WriteAllText(Path.Combine(@"../new_src", Path.GetFileName(sourceTree.FilePath)), newSource.ToFullString());
+                }
+            }
+        }
+
+        private static Compilation CreateTestCompilation()
+        {
+            // creation of the syntax tree for every file
+            String programPath = @"Program.cs";
+            String programText = File.ReadAllText(programPath);
+            SyntaxTree programTree = CSharpSyntaxTree.ParseText(programText)
+                                     .WithFilePath(programPath);
+            String rewriterPath = @"InitializerRewriter.cs";
+            String rewriterText = File.ReadAllText(rewriterPath);
+            SyntaxTree rewriterTree = CSharpSyntaxTree.ParseText(rewriterText)
+                                .WithFilePath(rewriterPath);
+            SyntaxTree[] sourceTrees = { programTree, rewriterTree };
+
+            // gathering the assemblies
+            MetadataReference mscorlib = MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location);
+            MetadataReference codeAnalysis = MetadataReference.CreateFromFile(typeof(SyntaxTree).GetTypeInfo().Assembly.Location);
+            MetadataReference csharpCodeAnalysis = MetadataReference.CreateFromFile(typeof(CSharpSyntaxTree).GetTypeInfo().Assembly.Location);
+            MetadataReference[] references = { mscorlib, codeAnalysis, csharpCodeAnalysis };
+
+            // compilation
+            return CSharpCompilation.Create("ConsoleApplication",
+                             sourceTrees,
+                             references,
+                             new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+        }
+    }
+}
+
